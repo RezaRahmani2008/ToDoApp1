550caaa9e8efe7bda3c0efa57bce361d73359969
Rio <Rioz2008x@gmail0com>
2023-05-18 14:19:56 +0330

    finish Notifiction

--- app/src/main/java/com/example/todoapp/AddTask.kt
+++ app/src/main/java/com/example/todoapp/AddTask.kt
@@ -1,12 +1,14 @@
 package com.example.todoapp
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.datastore.dataStore
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
@@ -16,14 +18,15 @@ import db.TodoListSerializer2
 import kotlinx.collections.immutable.mutate
 import kotlinx.coroutines.launch
 import model.ToDo
+import notifiction.Notifiction
 import utils.Picker
 import utils.fullDate
 import utils.hour
 import utils.minute
 
 
-
 val Context.dataStore by dataStore("mainFile.json", TodoListSerializer())
+
 class AddTask : Fragment() {
     private lateinit var binding: FragmentAddTaskBinding
     override fun onCreateView(
@@ -33,18 +36,21 @@ class AddTask : Fragment() {
         binding = FragmentAddTaskBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.hi.setOnClickListener {
-            Picker(parentFragmentManager,binding.hi)
+            Picker(parentFragmentManager, binding.hi)
         }
-        binding.AddTask.setOnClickListener{
+        binding.AddTask.setOnClickListener {
             val newToDo = ToDo(
                 binding.titleEditText.editText?.text.toString(),
                 binding.descriptionEditText.editText?.text.toString(),
-                "$hour:$minute", fullDate, false)
+                "$hour:$minute", fullDate, false
+            )
 
-            lifecycleScope.launch{
+            lifecycleScope.launch {
                 requireContext().dataStore.updateData {
                     it.copy(
                         it.todoList.mutate {
@@ -52,7 +58,15 @@ class AddTask : Fragment() {
                         }
                     )
                 }
-                Navigation.findNavController(binding.AddTask).navigate(R.id.action_addTask_to_correntToDos)
+                Notifiction(
+                    newToDo.title,
+                    newToDo.description,
+                    requireContext(),
+                    newToDo.hashCode().toString(),
+                    newToDo
+                )
+                Navigation.findNavController(binding.AddTask)
+                    .navigate(R.id.action_addTask_to_correntToDos)
 
             }
         }
